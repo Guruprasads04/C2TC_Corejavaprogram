b64841dd9ad2d3bda2169887a2659c59b5e33c74
Guruprasad <guruprasadss2004@gmail.com>
2025-07-21 18:37:42 +0530

    character Demo in java,ASCI value and unicode

---
+++ C2CT_corejavapogram/src/dayoneA1/charDemo.java
@@ -0,0 +1,18 @@
+package dayoneA1;
+
+public class charDemo {
+
+	public static void main(String[] args) {
+		char ch = 'b';
+		System.out.println(ch);
+		char ch1 = 68;
+		System.out.println(ch1);
+		char v1 = '\u08A7';
+		System.out.println(v1);
+		char v2 = '\u20AC';
+		System.out.println(v2);
+		int a = 'B';
+		System.out.println(a);
+	}
+
+}
