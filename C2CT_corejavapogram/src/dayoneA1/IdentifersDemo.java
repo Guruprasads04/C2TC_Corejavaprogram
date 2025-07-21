259e3cc0674376cd6c036136760bce52ea89395e
Guruprasad <guruprasadss2004@gmail.com>
2025-07-21 19:01:24 +0530

    identifiers in java

---
+++ C2CT_corejavapogram/src/dayoneA1/IdentifersDemo.java
@@ -0,0 +1,14 @@
+package dayoneA1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 15; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Guruprasad"; 
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
