9ddc3d6c049c672f4f75cc00247a49206dd51a99
Guruprasad <guruprasadss2004@gmail.com>
2025-07-21 14:33:18 +0530

    addition program

--- C2CT_corejavapogram/src/day1/basics/SampleDemo.java
+++ C2CT_corejavapogram/src/day1/basics/SampleDemo.java
@@ -1,11 +1,17 @@
 package day1.basics;
 
 public class SampleDemo {
-
-	public static void main(String[] args) {
-		System.out.println("hi hello guys....");
-		System.out.println("hi hello,sir guru morning....");
+	public static void main(String[] args){
+		int a,b,c;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		c=a+b;
+		System.out.println("addition of a="+a+" and b="+b+" is "+c);
+		
+	}
+	
+	
         
 	}
 
-}
+
