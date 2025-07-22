b82e4316aecbecec2ed7e6cf5496e925261c1837
Guruprasad <guruprasadss2004@gmail.com>
2025-07-22 14:38:29 +0530

    product details showCasing using getter and setteralo toString

---
+++ C2CT_corejavapogram/src/productA2/product.java
@@ -0,0 +1,31 @@
+package productA2;
+
+public class product {
+	private int pid;
+	private String item;
+	private String price;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	@Override
+	public String toString() {
+		return "product [pid=" + pid + ", item=" + item + ", price=" + price + "]";
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+
+}
---
+++ C2CT_corejavapogram/src/productA2/productDetails.java
@@ -0,0 +1,17 @@
+package productA2;
+
+public class productDetails {
+
+	public static void main(String[] args) {
+		 product p=new product();
+		    p.setPid(101);
+	    	p.setItem("brush");
+			p.setPrice("15"); 
+			
+			System.out.println("customer id :"+p.getPid());
+			System.out.println(" item :"+p.getItem());
+			System.out.println(" price :"+p.getPrice());
+			System.out.println(p);
+	}
+
+}
