f0e984b08cf76c56ece15870658607916c9910a6
Guruprasad <guruprasadss2004@gmail.com>
2025-07-22 20:35:34 +0530

    product details using getter|setter and toString

--- C2CT_corejavapogram/src/entityclassassignment2/productDetails.java
+++ C2CT_corejavapogram/src/entityclassassignment2/productDetails.java
@@ -3,15 +3,38 @@ package entityclassassignment2;
 public class productDetails {
 
 	public static void main(String[] args) {
-		 product p=new product();
-		    p.setPid(101);
-	    	p.setItem("brush");
-			p.setPrice("15"); 
+		    product p1=new product();
+		    p1.setPid(101);
+	    	p1.setItem("brush");
+			p1.setPrice("15"); 
 			
-			System.out.println("customer id :"+p.getPid());
-			System.out.println(" item :"+p.getItem());
-			System.out.println(" price :"+p.getPrice());
-			System.out.println(p); 
+			System.out.println("customer id :"+p1.getPid());
+			System.out.println(" item :"+p1.getItem());
+			System.out.println(" price :"+p1.getPrice());
+			System.out.println(p1); 
+			
+			
+	  
+	        product p1=new product();
+		    p1.setPid(102);
+	    	p1.setItem("toothpaste");
+			p1.setPrice("30"); 
+			
+			System.out.println("customer id :"+p1.getPid());
+			System.out.println(" item :"+p1.getItem());
+			System.out.println(" price :"+p1.getPrice());
+			System.out.println(p1); 
+			
+			
+			product p1=new product();
+			p1.setPid(103);
+	    	p1.setItem("biscuit");
+			p1.setPrice("40"); 
+			
+			System.out.println("customer id :"+p1.getPid());
+			System.out.println(" item :"+p1.getItem());
+			System.out.println(" price :"+p1.getPrice());
+			System.out.println(p1);  
 	}
 
 }
