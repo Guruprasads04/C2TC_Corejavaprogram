c162f53cf7f9199d8c5cba43d285ced0f4e5990d
Guruprasad <guruprasadss2004@gmail.com>
2025-07-22 20:36:41 +0530

    product details

--- C2CT_corejavapogram/src/entityclassassignment2/productDetails.java
+++ C2CT_corejavapogram/src/entityclassassignment2/productDetails.java
@@ -15,26 +15,26 @@ public class productDetails {
 			
 			
 	  
-	        product p1=new product();
-		    p1.setPid(102);
-	    	p1.setItem("toothpaste");
-			p1.setPrice("30"); 
+	        product p2=new product();
+		    p2.setPid(102);
+	    	p2.setItem("toothpaste");
+			p2.setPrice("30"); 
 			
-			System.out.println("customer id :"+p1.getPid());
-			System.out.println(" item :"+p1.getItem());
-			System.out.println(" price :"+p1.getPrice());
-			System.out.println(p1); 
+			System.out.println("customer id :"+p2.getPid());
+			System.out.println(" item :"+p2.getItem());
+			System.out.println(" price :"+p2.getPrice());
+			System.out.println(p2); 
 			
 			
-			product p1=new product();
-			p1.setPid(103);
-	    	p1.setItem("biscuit");
-			p1.setPrice("40"); 
+			product p3=new product();
+			p3.setPid(103);
+	    	p3.setItem("biscuit");
+			p3.setPrice("40"); 
 			
-			System.out.println("customer id :"+p1.getPid());
-			System.out.println(" item :"+p1.getItem());
-			System.out.println(" price :"+p1.getPrice());
-			System.out.println(p1);  
+			System.out.println("customer id :"+p3.getPid());
+			System.out.println(" item :"+p3.getItem());
+			System.out.println(" price :"+p3.getPrice());
+			System.out.println(p3);  
 	}
 
 }
