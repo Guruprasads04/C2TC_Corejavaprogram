0b05f8b120ad685c4b0f36e1cd5bb44109d65419
Guruprasad <guruprasadss2004@gmail.com>
2025-07-24 19:31:36 +0530

    multilevel inhertiance using vehicle (car)

---
+++ C2CT_corejavapogram/src/dayfive/multilevelinheritance/vehicle/car.java
@@ -0,0 +1,12 @@
+package dayfive.multilevelinheritance.vehicle;
+
+public class car {
+	public car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+}
