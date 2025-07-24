4207f0fdc9b8cfb94cf8fddc56e58fd72899931b
Guruprasad <guruprasadss2004@gmail.com>
2025-07-24 19:54:30 +0530

    single inheritance with student record

---
+++ C2CT_corejavapogram/src/dayfive/singleinheritance/Single_INTDemo.java
@@ -0,0 +1,11 @@
+package dayfive.singleinheritance;
+
+public class Single_INTDemo {
+
+	public static void main(String[] args) {
+		 Student student = new Student("GURUPRASAD", "345808080", "PONDY", 983467856, 18, "City College");
+		 System.out.println(student);
+
+	}
+
+}
