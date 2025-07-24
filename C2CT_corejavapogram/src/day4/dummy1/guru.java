a6227356899f98bc3bbe6f0b0ac5c9f48ebfbbea
Guruprasad <guruprasadss2004@gmail.com>
2025-07-24 15:21:19 +0530

    hi this is demo package folder test

---
+++ C2CT_corejavapogram/src/day4/dummy1/guru.java
@@ -0,0 +1,9 @@
+package day4.dummy1;
+
+public class guru {
+
+	public static void main(String[] args) {
+System.out.println("hi heloo");
+	}
+
+}
---
+++ C2CT_corejavapogram/src/dayfour/first_package/Base.java
@@ -0,0 +1,5 @@
+package dayfour.first_package;
+
+public class Base {
+
+}
