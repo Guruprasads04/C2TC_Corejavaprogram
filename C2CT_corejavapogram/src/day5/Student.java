9f02dc4fe5a97daf1892a072446f20da8bf2b11b
Guruprasad <guruprasadss2004@gmail.com>
2025-07-25 14:29:51 +0530

    this program is about the inheritance concept

---
+++ C2CT_corejavapogram/src/day5/Student.java
@@ -0,0 +1,35 @@
+package day5;
+
+public class Student extends Person{
+	private int uid;
+	private String coursename;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	@Override
+	public String toString() {
+		return "Student [student id: "+super.getPid() +" student name:\"+super.getName()+\" ,trainer city:\"+super.getCity()+ uid=" + uid + ", coursename=" + coursename + "]";
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int uid, String coursename) {
+		super();
+		this.uid = uid;
+		this.coursename = coursename;
+	}
+
+	
+	
+
+}
