b17d3a67b8594d79df124dbb41ded300e47daadc
Guruprasad <guruprasadss2004@gmail.com>
2025-07-31 15:51:02 +0530

    This file contains core java programs for day11

---
+++ C2CT_corejavapogram/src/dayEleven/V1/Demo.java
@@ -0,0 +1,11 @@
+package dayEleven.V1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+
+}
